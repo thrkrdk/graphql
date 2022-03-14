645d66886352819353af8b17d6d544c3316af7ad
thrkrdk <thrkardak@gmail.com>
2022-03-14 11:22:30 +0300

    Long Scalar  Added

--- src/main/java/com/graphql/springboot/graphql/config/ScalarConfig.java
+++ src/main/java/com/graphql/springboot/graphql/config/ScalarConfig.java
@@ -32,4 +32,44 @@ public class ScalarConfig {
     public GraphQLScalarType time() {
         return ExtendedScalars.Time;
     }
+    
+     @Bean
+    public GraphQLScalarType longScalar() {
+        return GraphQLScalarType.newScalar().name("Long").description("Java 8 Long as scalar.").coercing(new Coercing<Long, String>() {
+            @Override
+            public String serialize(final Object dataFetcherResult) {
+                if (dataFetcherResult instanceof Long) {
+                    return dataFetcherResult.toString();
+                } else {
+                    throw new CoercingSerializeException("Expected a Long object.");
+                }
+            }
+
+            @Override
+            public Long parseValue(final Object input) {
+                try {
+                    if (input instanceof String) {
+                        return Long.parseLong((String) input);
+                    } else {
+                        throw new CoercingParseValueException("Expected a String");
+                    }
+                } catch (Exception e) {
+                    throw new CoercingParseValueException(String.format("Not a valid Long: '%s'.", input), e);
+                }
+            }
+
+            @Override
+            public Long parseLiteral(final Object input) {
+                if (input instanceof StringValue) {
+                    try {
+                        return Long.parseLong((((StringValue) input).getValue()));
+                    } catch (Exception e) {
+                        throw new CoercingParseLiteralException(e);
+                    }
+                } else {
+                    throw new CoercingParseLiteralException("Expected a StringValue.");
+                }
+            }
+        }).build();
+    }
 }
